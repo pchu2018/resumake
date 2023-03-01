bfaa1912f3b39d777b7259e143051d8548949a4e
PEIYUN CHU <pchu2018@gmail.com>
2023-02-28 21:20:51 -0500

    initialized project with react skeleton
    Co-authored-by: arrickx <ar.rick@hotmail.com>
    
    Co-authored-by: marcoxulin <106225683+marcoxulin@users.noreply.github.com>
    Co-authored-by: codergirl01 <wendykuowob@gmail.com>

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// serve index.html on the route '/'
+app.get("/", (req, res) => {
+  return res.status(200).sendFile(path.join(__dirname, "../index.html"));
+});
+
+app.listen(3000);

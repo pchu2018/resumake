4618118f8ac8fd3ffa80a6263f0a5119a2ed9965
Eric Vartanian <eric.vartanian1@gmail.com>
2023-03-03 08:33:56 -0800

    sql file successfully populates database

--- resume_postgres_create.sql
+++ resume_postgres_create.sql
@@ -1,5 +1,8 @@
+--
+-- PostgreSQL database dump
+--
 
-CREATE TABLE user (
+CREATE TABLE app_user (
 "userId" serial NOT NULL,
 "name" varchar NOT NULL,
 "location" varchar NOT NULL,
@@ -13,30 +16,38 @@ CREATE TABLE resume (
 "resumeId" serial NOT NULL,
 "userId" int NOT NULL,
 CONSTRAINT "resume_pk" PRIMARY KEY ("resumeId")
-CONSTRAINT "fk_resume_user" 
-    FOREIGN KEY("userId")
-        REFERENCES user("userId")
 );
 
 CREATE TABLE component (
 "componentId" serial NOT NULL,
+"userId" int NOT NULL,
 "header" varchar NOT NULL,
 "bullets" varchar NOT NULL,
-CONSTRAINT "fk_component_user" 
-    FOREIGN KEY("userId")
-        REFERENCES user("userId"),
 CONSTRAINT "component_pk" PRIMARY KEY ("componentId")
 );
 
 CREATE TABLE grid (
 "gridId" serial NOT NULL,
+"resumeId" serial NOT NULL,
+"componentId" serial NOT NULL,
 "x_coordinate" int NOT NULL DEFAULT 0,
 "y_coordinate" int NOT NULL DEFAULT 0,
 CONSTRAINT "grid_pk" PRIMARY KEY ("gridId")
-CONSTRAINT "fk_grid_resume" 
-    FOREIGN KEY("resumeId")
-        REFERENCES user("resumeId"),
-CONSTRAINT "fk_grid_component" 
-    FOREIGN KEY("componentId")
-        REFERENCES user("componentId")
 );
+
+ALTER TABLE resume ADD CONSTRAINT "fk_resume_user" FOREIGN KEY("userId") REFERENCES app_user("userId");
+ALTER TABLE component ADD CONSTRAINT "fk_component_user" FOREIGN KEY("userId") REFERENCES app_user("userId");
+ALTER TABLE grid ADD CONSTRAINT "fk_grid_resume" FOREIGN KEY("resumeId") REFERENCES resume("resumeId");
+ALTER TABLE grid ADD CONSTRAINT "fk_grid_component" FOREIGN KEY("componentId") REFERENCES component("componentId");
+
+INSERT INTO app_user VALUES (1, 'George', 'London', 'LinkedIn.com/george', 'george@gmail.com', 'CTO');
+INSERT INTO app_user VALUES (2, 'Jerry', 'Paris', 'LinkedIn.com/jerry', 'jerry@gmail.com', 'CEO');
+
+INSERT INTO resume VALUES (1, 1);
+INSERT INTO resume VALUES (2, 2);
+
+INSERT INTO component VALUES (1, 1, 'cool header', 'sweet bullets');
+INSERT INTO component VALUES (2, 2, 'nice header', 'dope bullets');
+
+INSERT INTO grid VALUES (1, 1, 1, 2, 3);
+INSERT INTO grid VALUES (2, 2, 2, 4, 5);

27d4686289bfca45bdf264d3c5d36d51afdc88f1
Richter Lau <ar.rick@hotmail.com>
2023-03-04 14:00:35 -0500

    create throttle function
    Co-authored-by: pchu2018 <pchu2018@users.noreply.github.com>

--- client/containers/resumeContainer.tsx
+++ client/containers/resumeContainer.tsx
@@ -1,27 +1,10 @@
 import {DndContext, MouseSensor, useSensor, useSensors} from '@dnd-kit/core';
 import {arrayMove, SortableContext, useSortable} from '@dnd-kit/sortable';
 import {CSS} from '@dnd-kit/utilities';
-import {ReactNode, useState} from 'react';
+import {ReactNode, useState, useEffect, useRef, useMemo, useCallback} from 'react';
+import { throttle } from '../utils';
+import ResumeComponent from '../components/ResumeComponent';
 
-function SortableItem(props: {children?: ReactNode; id: string}) {
-  const {listeners, setNodeRef, transform, transition} = useSortable({
-    id: props.id,
-  });
-
-  const style = {
-    padding: '5vmin',
-    background: 'orange',
-    border: '2px solid',
-    transform: CSS.Transform.toString(transform),
-    transition,
-  } as React.CSSProperties;
-
-  return (
-    <div ref={setNodeRef} style={style} {...listeners}>
-      {props.children}
-    </div>
-  );
-}
 
 export default function ResumeContainer() {
   const [items, setItems] = useState(['asda', 'xz', '3', '4']);
@@ -33,9 +16,26 @@ export default function ResumeContainer() {
     }),
   );
 
+  const isInitialMount = useRef(true);
+
+  useEffect(() => {
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+   } else {
+       // Your useEffect code here to be run on update
+       throttledFetch(items);
+   }
+  }, [items])
+
+  // will change to fetch
+  const callback = (items: string[]) => console.log(items);
+
+  // memo-ize throttled function
+  const throttledFetch = useMemo(() => throttle(callback, 5000), [])
+
+  
   function handleDragEnd(event: any) {
     const {active, over} = event;
-    console.log('drag end!');
     if (over && active.id !== over.id) {
       setItems((items) => {
         const oldIndex = items.indexOf(active.id);
@@ -49,9 +49,7 @@ export default function ResumeContainer() {
     <DndContext sensors={sensors} onDragEnd={handleDragEnd}>
       <SortableContext items={items}>
         {items.map((id) => (
-          <SortableItem key={id} id={id}>
-            <p>hello from {id}</p>
-          </SortableItem>
+          <ResumeComponent key={id} id={id} content={`hello i am ${id}`} />
         ))}
       </SortableContext>
     </DndContext>
